afa0676b9a45d102b20cdfb6d013e1f439f84d53
chenzh <109347907@qq.com>
2021-10-07 18:56:39 +0800

    change menu

--- menu.py
+++ menu.py
@@ -6,4 +6,14 @@ def add_menu():
     main_menu = menus.find_menu('LevelEditor.MainMenu')  # 获取菜单控件
     added_menu = main_menu.add_sub_menu(main_menu.get_name(), 'new_menu', 'a', 'b')
 
+    entry = unreal.ToolMenuEntry(
+        name='entry_name',
+        type=unreal.MultiBlockType.MENU_ENTRY,
+        insert_position=unreal.ToolMenuInsert('', unreal.ToolMenuInsertType.DEFAULT)
+    )
+    entry.set_label('entry_label')
+    entry.set_string_command(unreal.ToolMenuStringCommandType.PYTHON, 'command_name', 'print("aaa")')
+
+    added_menu.add_menu_entry('section_name', entry)
+
     menus.refresh_all_widgets()
